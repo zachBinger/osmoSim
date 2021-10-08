01c786e7028b8313269238e04f2def5e5f6bd152
zachBinger <zbinger@email.arizona.edu>
2021-10-08 10:47:36 -0700

    initialization of file system

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Modeling software for osmotically-driven membrane processes such as reverse osmosis, forward osmosis, and pressure retarded osmosis',
+    author='Zachary Binger',
+    license='MIT',
+)
