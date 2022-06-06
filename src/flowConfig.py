117a44fca8ec9e308f127a068330bee58b5de797
zachBinger <zbinger@email.arizona.edu>
2022-06-06 15:04:19 -0700

    started to add concentration changes

--- src/flowConfig.py
+++ src/flowConfig.py
@@ -5,6 +5,7 @@ import itertools
 from itertools import permutations
 import matplotlib.pyplot as plt
 import math
+import transport
 
 class crossFlow:
     def __init__(self, props):
@@ -18,6 +19,30 @@ class crossFlow:
         for x, y in np.ndindex(self.domain.shape):
             self.domain[x,y]['Index'] = (x,y)
 
+    def adjustFlow(self,x,y,solver_ans,loop):
+        if loop == 1:
+            self.domain[y+1,x]['Inside Flow Rate'] = self.domain[y,x]['Inside Flow Rate'] + solver_ans
+            self.domain[y,x+1]['Outside Flow Rate'] = self.domain[y,x]['Outside Flow Rate'] - solver_ans
+        
+            self.domain[y+1,x]['Inside Concentration'] = self.domain[y,x]['Inside Concentration']*(self.domain[y,x]['Inside Flow Rate']*1000) \
+                /(self.domain[y+1,x]['Inside Flow Rate']*1000)
+            self.domain[y,x+1]['Outside Concentration'] = self.domain[y,x]['Outside Concentration']*(self.domain[y,x]['Outside Flow Rate']*1000) \
+                /(self.domain[y,x+1]['Outside Flow Rate']*1000)
+        
+        if loop == 2:
+            self.domain[y-1,x]['Inside Flow Rate'] = self.domain[y,x]['Inside Flow Rate'] + solver_ans
+            self.domain[y,x]['Outside Flow Rate'] = self.domain[y,x-1]['Outside Flow Rate'] - solver_ans
+
+        if loop == 'flip':
+            for x in range(0,int(self.properties["zones"]/2)):
+                solverAns = transport.FOeqns(module, (1,1))['perm_rate'][0]
+                self.domain[math.floor(self.properties["zones"]-1),x+1]['Outside Flow Rate'] = self.domain[math.floor(self.properties["zones"]-1),x]['Outside Flow Rate'] - solverAns
+        # Flip the values at the baffle turn
+            for idx in range(self.domain[self.properties["zones"]-1,int(self.properties["zones"]/2):].size):
+                self.domain[self.properties["zones"]-1,int(self.properties["zones"]/2+idx)]['Inside Flow Rate'] = self.domain[self.properties["zones"]-1,int(idx)]['Inside Flow Rate'] - solverAns
+                # self.domain[self.properties["zones"]-1,int(self.properties["zones"]/2+idx)]['Inside Concentration'] = self.domain[self.properties["zones"]-1,int(idx)]['Inside Concentration']
+                # self.domain[self.properties["zones"]-1,int(self.properties["zones"]/2+idx)]['Inside Flow Rate'] = self.domain[self.properties["zones"]-1,int(idx)]['Inside Flow Rate']
+        
     def iterate(self):
 
         # ## 1st loop
@@ -25,28 +50,22 @@ class crossFlow:
         for y in range(0,math.floor(self.properties["zones"]-1)):
             # #j direction will go along the length of the membrane and will initially stop at the baffle (half way)
             for x in range(0,int(self.properties["zones"]/2)):
-                solverAns = 0.1
-                self.domain[y+1,x]['Inside Flow Rate'] = self.domain[y,x]['Inside Flow Rate'] + solverAns
-                self.domain[y,x+1]['Outside Flow Rate'] = self.domain[y,x]['Outside Flow Rate'] - solverAns
+                solverAns = transport.FOeqns(module, (1,1))['perm_rate'][0]
+                crossFlow.adjustFlow(self,x,y,solverAns, 1)
 
-        for x in range(0,int(self.properties["zones"]/2)):
-            solverAns = 0.1
-            self.domain[math.floor(self.properties["zones"]-1),x+1]['Outside Flow Rate'] = self.domain[math.floor(self.properties["zones"]-1),x]['Outside Flow Rate'] - solverAns
-        # Flip the values at the baffle turn
-        for idx in range(self.domain[self.properties["zones"]-1,int(self.properties["zones"]/2):].size):
-            self.domain[self.properties["zones"]-1,int(self.properties["zones"]/2+idx)]['Inside Flow Rate'] = self.domain[self.properties["zones"]-1,int(idx)]['Inside Flow Rate']
+        # Flip across baffle
+        crossFlow.adjustFlow(self,x,y,0, 'flip')
 
         ## 2nd loop
         # i direction will go along the whole width of the membrane
         for y in range(self.properties["zones"]-1,0,-1):
             # j direction will go along the length of the membrane and will initially stop at the baffle (half way)
             for x in range(int(self.properties["zones"]/2),self.properties["zones"]):
-                solverAns = 0.1
-                self.domain[y-1,x]['Inside Flow Rate'] = self.domain[y,x]['Inside Flow Rate'] + solverAns
-                self.domain[y,x]['Outside Flow Rate'] = self.domain[y,x-1]['Outside Flow Rate'] - solverAns
+                solverAns = transport.FOeqns(module, (1,1))['perm_rate'][0]
+                crossFlow.adjustFlow(self,x,y,solverAns,2)
         
         for x in range(int(self.properties["zones"]/2),self.properties["zones"]):
-            solverAns = 0.1
+            solverAns = transport.FOeqns(module, (1,1))['perm_rate'][0]
             self.domain[0,x]['Outside Flow Rate'] = self.domain[0,x-1]['Outside Flow Rate'] - solverAns
 
     def visualize(self, side, char):
@@ -65,9 +84,9 @@ class crossFlow:
 module_properties = {
     "Width":                1,  # m
     "Length":               1,  # m
-    "zones":                100,  # quantity
-    "Water Permeability":   1,
-    "Salt Permeability":    1,
+    "zones":                10,  # quantity
+    "Water Permeability":   1e-9,
+    "Salt Permeability":    1e-6,
     "Defects":              0,
     "mode":                 'FO',
     
@@ -80,10 +99,14 @@ module_chars = {
     "Inside Flow Rate":         1,  # m3/s
     "Outside Concentration":    1,  # g/L
     "Inside Concentration":     32,  # g/L
+    "Outside Pressure":    10,  # Pa
+    "Inside Pressure":     10,  # Pa
 }
 
 module = crossFlow(module_properties)
 module.initialize(module_chars)
 module.iterate()
-module.visualize('Inside','Inside Flow Rate')
-module.visualize('Outside','Outside Flow Rate')
+# module.visualize('Inside','Inside Flow Rate')
+# module.visualize('Outside','Outside Flow Rate')
+module.visualize('Inside','Inside Concentration')
+module.visualize('Outside','Outside Concentration')
--- src/transport.py
+++ src/transport.py
@@ -1,15 +1,26 @@
 from scipy.optimize import fsolve
-import transport
+import waterProperties
 
 def FOeqns(module, zone):
 
-    def waterFlux(Jw):
-        return Jw - (module.properties["Water Permeability"]*(1)                    )
+    def solve_osm_transport(Jw):
+        feed_osmP = waterProperties.molarity(module.domain[zone]['Inside Concentration'])*2*8.314*298
+        draw_osmP = waterProperties.molarity(module.domain[zone]['Outside Concentration'])*2*8.314*298
 
-    print(fsolve(waterFlux,0))
+        del_conc = module.domain[zone]['Outside Concentration'] - module.domain[zone]['Inside Concentration']
+        del_pi = feed_osmP - draw_osmP
 
+        del_P = module.domain[zone]['Outside Pressure'] - module.domain[zone]['Inside Pressure']
+        
+        return Jw - (module.properties["Water Permeability"]*(del_pi - del_P))
 
+    Jw = fsolve(solve_osm_transport,0.1)
+    flux_lmh = Jw*(1000*3600)
+    perm_rate = (flux_lmh*((module.properties['Length']*module.properties['Width'])/module.properties['zones'])*24)/1000
+    
+    return {'Jw':Jw,'flux_lmh':flux_lmh,'perm_rate':perm_rate}
 
+    
 
 # module.initialize(module_chars)
 # module.iterate()
